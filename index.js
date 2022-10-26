8d7a63595a8091ce6821dd3e7d35033c22d2d939
BenCottrell <bencottrell48@yahoo.com>
2022-10-25 21:02:04 -0400

    created an array of questions to prompt for user input

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Give a description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions for your project?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is the usage information for your project?',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'Which of these licenses would you like to use?',
+        choices: ["MIT", "Apache", "BSD", "Unlicense"],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What are the contribution guidelines for your project?',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions for your project?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
